10d897fdb682363c9b31b6fbe0cfe22b2c56e882
Fib88 <nsukifrank@hotmail.com>
2020-09-30 17:21:17 +0200

    latest commit of day setting up for getting my evolutions

--- pokedex.php
+++ pokedex.php
@@ -12,16 +12,27 @@ if($pokemon === null){
     $pokemon = 1;
 }
 $pokemonData = file_get_contents("https://pokeapi.co/api/v2/pokemon/" .$pokemon);
-$poke = (json_decode($pokemonData,true));
+$pokemonSpeciesData = file_get_contents("https://pokeapi.co/api/v2/pokemon-species/" .$pokemon);
 
+$poke = (json_decode($pokemonData,true));
+$species = (json_decode($pokemonSpeciesData,true));
+
+
+
+$pokemonEvoUrl = $poke['species']['url'];
+var_dump($pokemonEvoUrl);
 $image = $poke['sprites']['front_default'];
 $moves = $poke['moves']['0']['move']['name'];
+$pokemonPreviousEvo = $species['evolves_from_species']['name'];
+$pokemonNextEvo = $species['evolution_chain'];
+var_dump($pokemonPreviousEvo);
+var_dump($pokemonNextEvo);
 
 
 
 function getRandomMoves($poke){
     $max = count($poke['moves']);
-    for ($i = 1; $i <=4; $i++) {
+    for ($i = 0; $i <=4; $i++) {
         $randomNumber = rand(0 ,$max);
         $randomMove = $poke['moves'][$randomNumber]['move']['name'];
         return $randomMove;
@@ -55,8 +66,11 @@ function getRandomMoves($poke){
 <div class="pokeName"><?php echo $poke['name'];?></div>
 <div class="pokeId"><?php echo $poke['id'];?></div>
 <img src="<?php echo $image;?>">
-<div class="pokeMoves"><?php echo getRandomMoves($poke)." ".getRandomMoves($poke)." ".getRandomMoves($poke)." ".getRandomMoves($poke);?></div>
-
+<h4>Moves:</h4>
+<p><?php echo getRandomMoves($poke);?></p>
+<p><?php echo getRandomMoves($poke);?></p>
+<p><?php echo getRandomMoves($poke);?></p>
+<p><?php echo getRandomMoves($poke);?></p>
 
 </body>
 </html>
